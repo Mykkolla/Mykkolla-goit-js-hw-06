948c86c73706172b68657a7b030a4d0b59e8ec78
Mykkolla <tarabukin.n.10@gmail.com>
2023-02-23 18:20:19 +0200

    add:

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,31 @@
+const mainForm = document.querySelector('.login-form');
 
+
+
+mainForm.addEventListener('submit', OnFormsub);
+
+
+function OnFormsub(elemen) {
+    elemen.preventDefault();
+    let NewObj = {};
+    const formElements = elemen.currentTarget.elements;
+// console.log(formElements);
+    const formElementsemail = formElements.email.value;
+    const formElementspassword = formElements.password.value;
+    
+    if (formElementsemail === '' && formElementspassword === '') {
+        alert('Всі поля повинні бути заповнені');
+    }
+    else {
+        NewObj[formElements.email.name] = formElementsemail;
+        NewObj[formElements.password.name] = formElementspassword;
+        
+        console.log(NewObj);
+    }
+
+}
+
+
+
+
+    
