8b957a1e7e5f42481443516be980d00de9e80769
Mykkolla <tarabukin.n.10@gmail.com>
2023-02-23 19:48:45 +0200

    add:

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const butChangeColor = document.querySelector('.change-color');
+const spanColorName = document.querySelector('.color');
+const Body = document.querySelector('body');
+
+
+butChangeColor.addEventListener('click', OnChangeColor);
+
+function OnChangeColor(elem) {
+  Body.style.backgroundColor = getRandomHexColor();
+  spanColorName.textContent = `${Body.style.backgroundColor}`
+}
+
